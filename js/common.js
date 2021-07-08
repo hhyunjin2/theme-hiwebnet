520170c24bb7e7a16dd3cd442a869843c74f98ea
현진 반 <bhj0212@naver.com>
2021-07-08 13:16:38 +0900

    공지사항 하루동안 보지 않기 기능 추가 쿠키생성 함수, 쿠키값 가져오는 함수 추가

--- js/common.js
+++ js/common.js
@@ -1,9 +1,47 @@
 $( () => {
 	
 
+	var NotShowNoticeCookie = Getcookie("NotshowNoticeToday");
+	if (NotShowNoticeCookie !== "")
+	{
+		$('#noticeModal').modal('hide');
+	}
+	else {
+		$('#noticeModal').modal('show');
+	}
 	// 공지사항 팝업
-	$('#noticeModal').modal('show');
 	
 	
+	$("#NotShowToday").on("click", function(){
+		var NotshowNoticeToday = "NotshowNoticeToday";
+		SetCookie(NotshowNoticeToday, "NotShowNoticeToday", 1);
+		$("#noticeModal").modal('hide');
+	});
 	
+	function Getcookie(cookieName)
+	{
+		// cookie값 가져오기
+		var cookieName = cookieName + "=";
+    	var ca = document.cookie.split(';');
+    	for (var i = 0; i < ca.length; i++) {
+        	var c = ca[i];
+        	while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cookieName) === 0) {
+            return c.substring(cookieName.length, c.length);
+        }
+    }
+    	return "";
+	}
+
+	function SetCookie(cookieName, value, exdays)
+	{
+		// cookie 값 생성
+		var day = new Date();
+		day.setTime(day.getTime() + exdays * 24 * 60 * 60 * 1000);
+		var expires = "expires=" + day.toUTCString();
+		document.cookie = cookieName + "=" + value + ";" + expires + ";path=/";
+
+	}
 });
